ea18b1345e2118369719e6a578814d28508b7d8a
Maniform <maniform.development@gmail.com>
2024-06-17 12:01:05 -0400

    Save and load muted audio and changed binary bad apple rate to float

--- Singletons/GameConfig.gd
+++ Singletons/GameConfig.gd
@@ -20,12 +20,18 @@ func load_config():
 	for audio_bus_index in range(0,AudioServer.bus_count - 1):
 		var audio_bus_name = AudioServer.get_bus_name(audio_bus_index)
 		var volume_db = config.get_value(audio_bus_name, "Volume")
-		AudioServer.set_bus_volume_db(audio_bus_index, volume_db)
+		if volume_db != null:
+			AudioServer.set_bus_volume_db(audio_bus_index, volume_db)
+		var is_bus_mute = config.get_value(audio_bus_name, "Mute")
+		if is_bus_mute != null:
+			AudioServer.set_bus_mute(audio_bus_index, is_bus_mute)
 		
 func save_congig():
 	for audio_bus_index in range(0,AudioServer.bus_count - 1):
 		var audio_bus_name = AudioServer.get_bus_name(audio_bus_index)
 		var volume_db = AudioServer.get_bus_volume_db(audio_bus_index)
+		var is_bus_mute = AudioServer.is_bus_mute(audio_bus_index)
 		config.set_value(audio_bus_name, "Volume", volume_db)
+		config.set_value(audio_bus_name, "Mute", is_bus_mute)
 	
 	config.save("user://game_config.cfg")
--- Sprites/AppleSpawner/AppleSpawner.gd
+++ Sprites/AppleSpawner/AppleSpawner.gd
@@ -1,5 +1,6 @@
 class_name AppleSpawner extends AnimatedSprite2D
 
+@export var bad_apple_rate := 0.5 as float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,4 +11,4 @@ func _process(delta):
 	pass
 
 func change():
-	frame = randi_range(0, 1)
+	frame = randf() < bad_apple_rate
